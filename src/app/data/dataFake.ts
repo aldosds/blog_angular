2e6275e8d10d92e47aec9c3b27a3fdcd7143d36c
Aldo Santos <aldoacesso@gmail.com>
2023-10-03 12:56:16 -0300

    Deixando a content page dinâmica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://cinepop.com.br/wp-content/uploads/2019/11/tony-stark.jpg",
+        "title": "MINHA NOTICIA",
+        "description": "Olá mundo!"
+    },
+    {
+        "id": "2",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "Siau novas séries",
+        "description": "Top 100"
+    },
+    {
+        "id": "3",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg",
+        "title": "Siau novo filme",
+        "description": "Adrenalina do começo ao fim"
+    },
+    {
+        "id": "4",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2019/08/x-men.jpg",
+        "title": "Novo filme dos X-Men",
+        "description": "Top 100"
+    }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover:string = "https://cinepop.com.br/wp-content/uploads/2019/11/tony-stark.jpg"
-  contentTitle:string = "MINHA NOTICIA"
-  contentDescription: string = "Olá mundo!"
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription: string = ""
 
-  constructor() { }
+  private id: string | null = "0"
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+    
+    //Pegando o conteudo em dataFake.ts
+    this.photoCover = result.photoCover
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+
   }
 
 }
