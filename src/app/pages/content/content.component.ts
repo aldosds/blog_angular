efceb00f1e4af889c74a99c7f8ac6a533f1e33db
Aldo Santos <aldoacesso@gmail.com>
2023-10-02 23:19:23 -0300

    Deixando o content

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  photoCover:string = "https://cinepop.com.br/wp-content/uploads/2019/11/tony-stark.jpg"
+  contentTitle:string = "MINHA NOTICIA"
+  contentDescription: string = "Olá mundo!"
+
   constructor() { }
 
   ngOnInit(): void {
